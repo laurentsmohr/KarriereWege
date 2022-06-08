99d37b940a47259ef780cc5c0d3de5497f61fece
Laurents Mohr <laurents.mohr@gmail.com>
2022-06-08 17:36:23 +0200

    Basic db operations

--- server/db.js
+++ server/db.js
@@ -1,25 +1,24 @@
 const { MongoClient } = require('mongodb');
 
-async function main() {
-  const pw = 'gKFomHZbZ1PQbafx';
-  // we'll add code here soon
-  const uri = `mongodb+srv://karrierewege:${pw}@cluster0.rlk2goy.mongodb.net/?retryWrites=true&w=majority`;
+const dbName = 'sample_airbnb';
+const pw = 'gKFomHZbZ1PQbafx';
+const uri = `mongodb+srv://karrierewege:${pw}@cluster0.rlk2goy.mongodb.net/?retryWrites=true&w=majority`;
 
+async function main() {
   const client = new MongoClient(uri);
 
   try {
     // Connect to the MongoDB cluster
     await client.connect();
 
-    await createListing(client, {
-      name: 'Lovely Loft',
-      summary: 'A charming loft in Paris',
-      bedrooms: 1,
-      bathrooms: 1,
-    });
-
     // Make the appropriate DB calls
-    await listDatabases(client);
+    // await createListing(client, {
+    //   name: 'Lovely Loft',
+    //   description: 'simple and nice',
+    //   bedrooms: 1,
+    //   bathrooms: 1,
+    // });
+    await findOneListingByName(client, 'Lovely Loft');
   } catch (e) {
     console.error(e);
   } finally {
@@ -30,10 +29,7 @@ async function main() {
 main().catch(console.error);
 
 async function createListing(client, newListing) {
-  const result = await client
-    .db('sample_airbnb')
-    .collection('listingAndReviews')
-    .insertOne(newListing);
+  const result = await client.db(dbName).collection('listingsAndReviews').insertOne(newListing);
   console.log(`New listing created with the following id: ${result.insertedId}`);
 }
 
@@ -42,4 +38,18 @@ async function listDatabases(client) {
 
   console.log('Databases:');
   databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+}
+
+async function createMultipleListings(client, newListings) {
+  const results = client.db(dbName).collection('listingsAndReviews').insertMany(newListings);
+  console.log(results.insertedIds);
+}
+
+async function findOneListingByName(client, nameOfListing) {
+  const result = await client
+    .db(dbName)
+    .collection('listingsAndReviews')
+    .findOne({ name: nameOfListing });
+
+  result ? console.log(result) : console.log('No listings found');
 }
